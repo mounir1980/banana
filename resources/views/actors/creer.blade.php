ee2a625ea5cb3e3ba3de4105fbc5facf5e0337e1
System Administrator <root@imac-de-utilisateur.home>
2016-03-22 20:32:25 +0100

    mardi21maison

--- resources/views/actors/creer.blade.php
+++ resources/views/actors/creer.blade.php
@@ -1,80 +1,82 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>creer</title>
+@extends('layout')
+@section('content')
 
 
-    <link href='https://fonts.googleapis.com/css?family=Schoolbell' rel='stylesheet' type='text/css'>
 
     <style>
-        html, body {
-            height: 100%;
+
+
+        .container
+        {
+            margin-left: 25%;
+            margin-top: 25%;
+
         }
 
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Schoolbell', cursive;
-            color: purple;
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
-        }
-
-        form{
-            width: 15%;
-        }
     </style>
-</head>
-<body>
-
 <div class="container">
-    <div class="content">
-        <div class="title">Acteur</div>
+    <div class="row">
+       <div class="col-md-6">
         <form method="post"
               action="{{route('actors_enregistrer')}}">
             {{--champ caché--}}
             {{csrf_field()}}
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="lastname"></label>
+                                <input name="lastname" id="lastname" placeholder="Lastname" class="form-control" />
+                            </div>
+                        </div>
 
-            <label for="biography">Biography:</label>
-            <input name="biography" id="biography" />
-            <br/>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="firstname"></label>
+                                <input name="firstname" id="firstname" placeholder="Firstname" class="form-control" />
+                            </div>
+                        </div>
+                    </div>
 
-            <label for="lastname">Lastname:</label>
-            <input name="lastname" id="lastname" />
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="biography"></label>
+                                <input name="biography" id="biography" placeholder="Biography" class="form-control" />
+                            </div>
+                        </div>
+                    </div>
 
-            <br>
-            <label for="firstname">Firstname:</label>
-            <input name="firstname" id="firstname" />
-            <br>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="city"></label>
+                                <input name="city" id="city" placeholder="City" class="form-control"/>
+                            </div>
+                        </div>
+                    </div>
 
-            <label for="city">City:</label>
-            <input name="city" id="city" />
-            <br>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
 
-            <label for="image">Image:</label>
-            <input name="image" id="image" />
-            <br>
+                                <label for="image"></label>
+                                <input type="file" capture="capture" accept="image/*" name="image" id="image"   />
+                            </div>
+                        </div>
+                    </div>
 
-            <p><button type="submit">Ok!</button></p>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <button type="submit" style="width: 100%" class="form-control">VALIDER</button>
+                            </div>
+                        </div>
+                    </div>
+
+
 
         </form>
+       </div>
     </div>
 </div>
-</body>
-</html>
+@endsection
